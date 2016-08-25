0a11484597f74febe8f3248d341f91d031116084
zcl <zhuchenglong123@gmail.com>
2016-08-25 18:57:35 -0400

    init my project

---
+++ learnGit/learnGit/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  learnGit
+//
+//  Created by zhuchenglong on 16/8/25.
+//  Copyright © 2016年 zcl. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
